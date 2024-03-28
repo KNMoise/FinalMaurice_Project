91bfef5cff3da24d83af5f09acaf309158158c1e
Irumva shumbusho <kentmars2002@gmail.com>
2024-03-28 20:45:32 +0200

    Color Fix V3.0

--- includes/choose.php
+++ includes/choose.php
@@ -17,20 +17,7 @@
   <link href="assets/vendor/fontawesome/css/fontawesome-all.min.css" rel="stylesheet" />
   <!-- Fables Icons -->
   <link href="assets/custom/css/fables-icons.css" rel="stylesheet" />
-  <!-- Bootstrap CSS -->
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
-  <link href="assets/vendor/bootstrap/css/bootstrap-4-navbar.css" rel="stylesheet" />
-  <!-- portfolio filter gallery -->
-  <link href="assets/vendor/portfolio-filter-gallery/portfolio-filter-gallery.css" rel="stylesheet" />
-  <!-- FANCY BOX -->
-  <link href="assets/vendor/fancybox-master/jquery.fancybox.min.css" rel="stylesheet" />
-  <!-- RANGE SLIDER -->
-  <link href="assets/vendor/range-slider/range-slider.css" rel="stylesheet" />
-  <!-- OWL CAROUSEL  -->
-  <link href="assets/vendor/owlcarousel/owl.carousel.min.css" rel="stylesheet" />
-  <link href="assets/vendor/owlcarousel/owl.theme.default.min.css" rel="stylesheet" />
-  <!-- FABLES CUSTOM CSS FILE -->
-  <link href="assets/custom/css/custom.css" rel="stylesheet" />
+
   <!-- FABLES CUSTOM CSS RESPONSIVE FILE -->
   <link href="assets/custom/css/custom-responsive.css" rel="stylesheet" />
 </head>
