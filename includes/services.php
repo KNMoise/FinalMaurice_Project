8d790665498d14743df5a8ac4f7558be74130220
KNMoise <obiomoise@gmail.com>
2024-03-28 19:56:36 +0200

    Update index.php title and services.php hover effect; modify slider.php structure
    * index.php: Change title to "Home - InnovateWithUs"
    * services.php: Adjust hover effect on service items
    * slider.php: Reorganize HTML structure for better readability and maintainability

--- includes/services.php
+++ includes/services.php
@@ -9,8 +9,7 @@
       </div>
       <div class="row g-4">
         <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-          <div
-            class="service-item d-flex flex-column text-center rounded">
+          <div class="service-item d-flex flex-column text-center rounded">
             <div class="service-icon flex-shrink-0">
               <i class="bi bi-lightbulb-fill fa-2x"></i>
             </div>
@@ -20,9 +19,7 @@
               Office Consultancy, offering tailored guidance for efficient
               project delivery and software utilization.
             </p>
-            <a class="btn btn-square" href="consmore.php "
-              ><i class="fa fa-arrow-right"></i
-            ></a>
+            <a class="btn btn-square" href="consmore.php"><i class="fa fa-arrow-right"></i></a>
           </div>
           <div id="popupCard1">
             <h5 style="text-align: center; color: white">Consultancy</h5>
@@ -32,14 +29,8 @@
             </p>
           </div>
         </div>
-
         <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
-          <div
-            class="service-item d-flex flex-column text-center rounded"
-            id="openPopupBtn2"
-            onmouseenter="showPopup('popupCard2')"
-            onmouseleave="hidePopup('popupCard2')"
-          >
+          <div class="service-item d-flex flex-column text-center rounded" id="openPopupBtn2" onmouseenter="showPopup('popupCard2')" onmouseleave="hidePopup('popupCard2')">
             <div class="service-icon flex-shrink-0">
               <i class="bi bi-file-earmark-binary-fill fa-2x"></i>
             </div>
@@ -51,9 +42,7 @@
               digitization services for efficient document organization and
               management.
             </p>
-            <a class="btn btn-square" href="docmore.php"
-              ><i class="fa fa-arrow-right"></i
-            ></a>
+            <a class="btn btn-square" href="docmore.php"><i class="fa fa-arrow-right"></i></a>
           </div>
           <div id="popupCard2">
             <h5 style="text-align: center; color: white">
@@ -68,12 +57,7 @@
           </div>
         </div>
         <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-          <div
-            class="service-item d-flex flex-column text-center rounded"
-            id="openPopupBtn3"
-            onmouseenter="showPopup('popupCard3')"
-            onmouseleave="hidePopup('popupCard3')"
-          >
+          <div class="service-item d-flex flex-column text-center rounded" id="openPopupBtn3" onmouseenter="showPopup('popupCard3')" onmouseleave="hidePopup('popupCard3')">
             <div class="service-icon flex-shrink-0">
               <i class="bi bi-arrow-clockwise fa-2x"></i>
             </div>
@@ -83,9 +67,7 @@
               through digital transformation, focusing on developing robust
               digital strategies aligned with business objectives.
             </p>
-            <a class="btn btn-square" href="read.php"
-              ><i class="fa fa-arrow-right"></i
-            ></a>
+            <a class="btn btn-square" href="read.php"><i class="fa fa-arrow-right"></i></a>
           </div>
           <div id="popupCard3">
             <h5 style="text-align: center; color: white">
--- includes/slider.php
+++ includes/slider.php
@@ -1,88 +1,79 @@
 <div class="slide-image-bg-area">
-      <section class="custom-hero-slider-area">
-        <div class="slide-front-image-bg-area">
-          <div class="hero-slider-wrap owl-carousel owl-theme">
-            <div class="slider-item slider-item-bg-1">
-              <div class="d-table">
-                <div class="d-table-cell">
-                  <div class="container">
-                    <div class="slider-text one">
-                      <span class="preheading"
-                        >Empower Your Business</span
-                      >
-                      <h1>Consultancy.</h1>
-                      <p>
-                        Our consultancy services provide strategic solutions to drive growth,
-                        optimize processes, and achieve your business objectives through software
-                        consultancy and project management office consultancy.
-                      </p>
+  <section class="custom-hero-slider-area">
+    <div class="slide-front-image-bg-area">
+      <div class="hero-slider-wrap owl-carousel owl-theme">
+        <div class="slider-item slider-item-bg-1">
+          <div class="d-table">
+            <div class="d-table-cell">
+              <div class="container">
+                <div class="slider-text one">
+                  <span class="preheading">Empower Your Business</span>
+                  <h1>Consultancy.</h1>
+                  <p>
+                    Our consultancy services provide strategic solutions to drive growth,
+                    optimize processes, and achieve your business objectives through software
+                    consultancy and project management office consultancy.
+                  </p>
 
-                      <div class="slider-btn">
-                        <a class="default-btn" href="tlc-insurance.html">
-                          Read more
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="slider-item slider-item-bg-2">
-              <div class="d-table">
-                <div class="d-table-cell">
-                  <div class="container">
-                    <div class="slider-text two">
-                      <span class="preheading"
-                        >Unlock Your Digital Potential</span
-                      >
-                      <h1>Digital Transformation</h1>
-                      <p>
-                        We offer expert consultation, strategy development, implementation,
-                        change management, process improvement, and workflow automation to 
-                        guide your digital transformation journey.
-                      </p>
-
-                      <div class="slider-btn">
-                        <a class="default-btn" href="auto-insurance.html">
-                          Read more
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="slider-item slider-item-bg-3">
-              <div class="d-table">
-                <div class="d-table-cell">
-                  <div class="container">
-                    <div class="slider-text two">
-                      <span class="preheading"
-                        >Organize, Secure, and Streamline Your Documents</span
-                      >
-                      <h1>Documents Management System</h1>
-                      <p>
-                        Our solutions include EDMS, digitization services, 
-                        records management consultancy, and training to help you
-                        capture, organize, store, and securely manage your electronic documents.
-                      </p>
-
-                      <div class="slider-btn">
-                        <a
-                          class="default-btn"
-                          href="general-liability-insurance.html"
-                        >
-                          Read more
-                        </a>
-                      </div>
-                    </div>
+                  <div class="slider-btn">
+                    <a class="default-btn" href="consmore.php">
+                      Read more
+                    </a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+
+        <div class="slider-item slider-item-bg-2">
+          <div class="d-table">
+            <div class="d-table-cell">
+              <div class="container">
+                <div class="slider-text two">
+                  <span class="preheading">Unlock Your Digital Potential</span>
+                  <h1>Digital Transformation</h1>
+                  <p>
+                    We offer expert consultation, strategy development, implementation,
+                    change management, process improvement, and workflow automation to
+                    guide your digital transformation journey.
+                  </p>
+
+                  <div class="slider-btn">
+                    <a class="default-btn" href="read.php">
+                      Read more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="slider-item slider-item-bg-3">
+          <div class="d-table">
+            <div class="d-table-cell">
+              <div class="container">
+                <div class="slider-text two">
+                  <span class="preheading">Organize, Secure, and Streamline Your Documents</span>
+                  <h1>Documents Management System</h1>
+                  <p>
+                    Our solutions include EDMS, digitization services,
+                    records management consultancy, and training to help you
+                    capture, organize, store, and securely manage your electronic documents.
+                  </p>
+
+                  <div class="slider-btn">
+                    <a class="default-btn" href="docmore.php">
+                      Read more
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </section>
+</div>
