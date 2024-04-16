e88820d3cb17d99852b8084a5018ed5525d77d1f
Irumva shumbusho <kentmars2002@gmail.com>
2024-04-16 21:02:02 +0200

    Done Contact us Form

--- contact.php
+++ contact.php
@@ -58,30 +58,30 @@
                                                 <div class="row g-3">
                                                     <div class="col-md-6">
                                                         <div class="form-floating">
-                                                            <input type="text" class="form-control" name="name" id="name" placeholder="Your Name">
+                                                            <input type="text" class="form-control" name="name" id="name" placeholder="Your Name" required>
                                                             <label for="name">Your Name</label>
                                                         </div>
                                                     </div>
                                                     <div class="col-md-6">
                                                         <div class="form-floating">
-                                                            <input type="email" class="form-control"  name="email" id="email" placeholder="Your Email">
+                                                            <input type="email" class="form-control"  name="email" id="email" placeholder="Your Email" required>
                                                             <label for="email">Your Email</label>
                                                         </div>
                                                     </div>
                                                     <div class="col-12">
                                                         <div class="form-floating">
-                                                            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+                                                            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
                                                             <label for="subject">Subject</label>
                                                         </div>
                                                     </div>
                                                     <div class="col-12">
                                                         <div class="form-floating">
-                                                            <textarea name="message" class="form-control" placeholder="Leave a message here" id="message" style="height: 150px"></textarea>
+                                                            <textarea name="message" class="form-control" placeholder="Leave a message here" id="message" style="height: 150px" required></textarea>
                                                             <label for="message">Message</label>
                                                         </div>
                                                     </div>
                                                     <div class="col-12">
-                                                        <button class="btn btn-primary w-100 py-3" type="submit">Send Message</button>
+                                                        <button class="btn btn-primary w-100 py-3" type="submit" name="contactUsForm">Send Message</button>
                                                     </div>
                                                 </div>
                                             </form>
